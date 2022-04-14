c012369d89d59b18242a70ba2fa8cf6dea72fc36
nidhidav <nidhisd2@illinois.edu>
2022-04-14 14:24:40 -0700

    code style changes

--- R Programming/R_Programming.R
+++ R Programming/R_Programming.R
@@ -8,14 +8,19 @@ rownames(board) <- c("row 1", "row 2", "row 3")
 colnames(board) <- c("col 1", "col 2", "col 3")
 print(board)
 
+if (interactive()) {
+  con <- stdin()
+} else {
+  con <- "stdin"
+}
 #function to place computer's move
 comp_move <- function (value, myboard) {
   assigned <- FALSE
-  while (assigned == FALSE) {
-    row = sample(1:3,1)
-    col = sample(1:3, 1)
+  while (!assigned) {
+    row <- sample(1:3,1)
+    col <- sample(1:3, 1)
     if (myboard[row,col] == "NA") {
-      myboard[row,col] = value
+      myboard[row,col] <- value
       assigned <- TRUE
     }
   }
@@ -24,58 +29,34 @@ comp_move <- function (value, myboard) {
   cat(" \n")
   return(myboard)
 }
-#function to ask row
-ask_row <- function(){
+#function to ask for row and col
+ask_rowcol <- function(){
   valid <- FALSE
-  while(valid == FALSE) {
-    if (interactive()) {
-      con <- stdin()
-    } else {
-      con <- "stdin"
-    }
+  while(!valid){
     cat("What row? ")
-    row_pos <- readLines(con = con, n = 1)
+    row_pos <- readLines(con <- con, n <- 1)
     row_pos <- as.numeric(row_pos)
-    if(row_pos %in% c(1,2,3)) {
-      valid <- TRUE
-    } else {
-      cat("Row value is not valid, please select from 1, 2, or 3", " \n")
-    }
-  }
-  return(row_pos)
-}
-#function to ask column
-ask_col <- function(){
-  valid <- FALSE
-  while(valid == FALSE) {
-    if (interactive()) {
-      con <- stdin()
-    } else {
-      con <- "stdin"
-    }
     cat("What col? ")
-    col_pos <- readLines(con = con, n = 1)
+    col_pos <- readLines(con <- con, n <- 1)
     col_pos <- as.numeric(col_pos)
-    if(col_pos %in% c(1,2,3)) {
-      valid <- TRUE
+    if ( !(row_pos %in% (1:3)) | !(col_pos %in% (1:3)) ) {
+      cat("Row value or col value is not valid, please select from 1, 2, or 3", " \n")
     } else {
-      cat("Column value is not valid, please select from 1, 2, or 3", " \n")
+      valid <- TRUE
     }
   }
-  return(col_pos)
+  return(c(row_pos, col_pos))
 }
-
 #function to place player's move
 player_move <- function(value, myboard){
   cat(value, "'s turn!", "\n")
   #check if viable position
   valid_pos <- FALSE
-  while (valid_pos == FALSE) {
-    row_pos <- ask_row()
-    col_pos <- ask_col()
-    if(myboard[row_pos,col_pos] == "NA") {
+  while (!valid_pos) {
+    row_col <- ask_rowcol()
+    if(myboard[row_col[1], row_col[2]] == "NA") {
       valid_pos <- TRUE
-      myboard[row_pos,col_pos] <- value
+      myboard[row_col[1], row_col[2]] <- value
     } else {
       cat("That place is already taken, please re-enter the row and col", "\n")
       valid_pos <- FALSE
@@ -86,28 +67,20 @@ player_move <- function(value, myboard){
 }
 
 #function to check if game ends
-winner <- "no one"
 game_end <- function(myboard) {
+  # browser()
+  winner <- "no one"
   #need to check three in a row that isn't NA 
   #returns columns + rows w three in a row, and values of those columns/rows
   same_col <- which(apply(myboard, 2, function(x) length(unique(x))) == 1)
   same_row <- which(apply(myboard, 1, function(x) length(unique(x))) == 1)
   #end game if rows are not NA (X or O)
-  if (any(myboard[,same_col]!="NA")) {
-    winner <- myboard[1,same_col]
-    for (x in winner) {
-      if (x != 'NA') {
-        winner <- x
-      }
-    }
+  #returns either an integer, or set of integers showing cols with three in a row (including NA)
+  if (length(same_col) > 0 & any(myboard[,same_col]!="NA")) {
+    winner <- myboard[1, which(myboard[1, same_col] != "NA")]
   }
-  if (any(myboard[same_row,]!="NA")) {
-    winner <- myboard[same_row,1]
-    for (x in winner) {
-      if (x != 'NA') {
-        winner <- x
-      }
-    }
+  if (length(same_row) > 0 & any(myboard[same_row,]!="NA")) {
+    winner <- myboard[which(myboard[same_row, 1] != "NA"), 1]
   }
   #check diagonal
   if (all(sapply(list(myboard[1,1],myboard[2,2],myboard[3,3]), function(x) x == myboard[3,3])) | all(sapply(list(myboard[1,3],myboard[2,2],myboard[3,1]), function(x) x == myboard[3,1]))){
@@ -116,7 +89,7 @@ game_end <- function(myboard) {
     }
   }
   #check tie - no NA in the board
-  if (any(myboard=="NA") == FALSE  & winner == "no one") {
+  if (!any(myboard=="NA")  & winner == "no one") {
     winner<- "tie"
   }
   return(winner)
@@ -127,12 +100,7 @@ name_player <- function(){
   name_valid <- FALSE
   cat("X or O? ")
   while (!name_valid){
-    if (interactive()) {
-      con <- stdin()
-    } else {
-      con <- "stdin"
-    }
-    myplayer <- readLines(con = con, n = 1)
+    myplayer <- readLines(con <- con, n <- 1)
     cat("\n")
     if (myplayer == "X" | myplayer == "O") {
       name_valid <- FALSE
@@ -144,22 +112,22 @@ name_player <- function(){
   }
 }
 
-player = name_player()
+player <- name_player()
 
 if (player == 'X') {
   computer <- 'O'
 } else {
   computer <- 'X'
-  board = comp_move(computer,board)
+  board <- comp_move(computer,board)
 }
 #player and computer take turns playing till 3 in a row or a tie
 repeat { 
-  board = player_move(player,board)
+  board <- player_move(player,board)
   #check if game ends
   if (game_end(board) != "no one") {
     break
   }
-  board = comp_move(computer,board)
+  board <- comp_move(computer,board)
   #check if game ends
   if (game_end(board) != "no one") {
     break
@@ -173,3 +141,4 @@ if (game_end(board) == "tie") {
 } else {
   cat("The winner is", game_end(board), "\n")
 }
+
